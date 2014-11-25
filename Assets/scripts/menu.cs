8cfe255408ae3d581936e93879090ee0edcf8d92
Arteom73 <artem9573@mail.ru>
2014-11-26 02:08:21 +0300

    preFinal

---
+++ Assets/scripts/menu.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class menu : MonoBehaviour {
+
+	void newgame(){
+		Application.LoadLevel("level1");
+	}
+	
+	void levels(){
+		Application.LoadLevel("levels");
+	}
+	
+	void exit(){
+		Application.Quit();
+	}
+	
+	void level1(){
+		Application.LoadLevel("level1");
+	}
+	
+	void level2(){
+		Application.LoadLevel("level2");
+	}
+	
+	void back(){
+		Application.LoadLevel("mainMenu");
+	}
+}
